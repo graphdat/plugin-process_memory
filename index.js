bcb2f89b3c7a53f3cecb1c9524d54a2f24970c7e
pdiemert <pete_diemert@msn.com>
2013-11-18 16:05:53 -0800

    findproc

--- index.js
+++ index.js
@@ -4,91 +4,19 @@ var _fs = require('fs');
 var _sysconf = require('sysconf');
 var _tools = require('graphdat-plugin-tools');
 
-function silent(fnc)
-{
-	try
-	{
-		return fnc();
-	}
-	catch(ex)
-	{
-		return null;
-	}
-}
-
-// Returns process id or 0 if not found, -1 if ambiguous, sets reason
-var reason;
-function findProcId(cfg)
-{
-	// Get all proc id's
-	var procs = _fs.readdirSync('/proc').filter(function(e) { return !isNaN(parseInt(e)); });
-	var pidResult = 0;
-
-	var hit;
-
-	procs.every(function(pid)
-	{
-		var stat = _fs.readFileSync('/proc/' + pid + '/stat', 'utf8').split(' ');
-		var cwd = silent(function() { return _fs.readlinkSync('/proc/' + pid + '/cwd'); });
-		var path = silent(function() { return _fs.readlinkSync('/proc/' + pid + '/exe'); });
-
-		var re;
-
-		var prc = {
-			name : stat[1].substr(1, stat[1].length - 2),
-			path : path || '',
-			cwd : cwd || ''
-		};
-
-		if (cfg.processName)
-		{
-			re = new RegExp(cfg.processName);
-			if (!re.test(prc.name))
-				return true;
-		}
-		if (cfg.processPath)
-		{
-			re = new RegExp(cfg.processPath);
-			if (!re.test(prc.path))
-				return true;
-		}
-		if (cfg.processCwd)
-		{
-			re = new RegExp(cfg.processCwd);
-			if (!re.test(prc.cwd))
-				return true;
-		}
-
-		// Got a hit, make sure not ambiguous
-		if (pidResult)
-		{
-			reason = 'process ambiguity: ' + JSON.stringify(prc) + ' is too similar to ' + JSON.stringify(hit);
-			pidResult = -1;
-			return false;
-		}
-		else
-		{
-			hit = prc;
-			pidResult = pid;
-			return true;
-		}
-	});
-
-	if (!pidResult)
-		reason = 'the process was not found';
-
-	return pidResult;
-}
-
-
 var _pollInterval = _param.pollInterval || 1000;
 var _pagesize = _sysconf.get(_sysconf._SC_PAGESIZE);
 
-
 function pollProcess(prc)
 {
+	var pidinfo;
+
 	if (!prc.pid || prc.pid <= 0)
-		prc.pid = findProcId(prc);
+	{
+		pidinfo = _tools.findProcId(prc);
+		prc.pid = pidinfo.pid;
+
+	}
 
 	if (prc.pid <= 0)
 	{
@@ -96,7 +24,7 @@ function pollProcess(prc)
 		if (!prc.notified)
 		{
 			prc.notified = true;
-			console.error('Unable to locate process for ' + prc.source + ', ' + reason);
+			console.error('Unable to locate process for ' + prc.source + ', ' + pidinfo.reason);
 		}
 	}
 	else
